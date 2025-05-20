8c675de499518104cdd5d080b77e8d3ca8b5fb8d
Mayzihardiputra <eziee496@gmail.com>
2025-05-20 10:40:15 +0700

    case3

--- stcakUsingLinkedList.cpp
+++ stcakUsingLinkedList.cpp
@@ -104,6 +104,17 @@ int main()
                 cout << "Stact is Empty. Cannnot pop.";
                 cout << endl;
             }
+            break;
+        case 3:
+            if (!stact.isEmpty())
+            {
+                stact.peek();
+            }
+            else
+            {
+                cout << "Stact is Empty. No top value." << endl;
+            }
+            break;
         }
     }
 }
